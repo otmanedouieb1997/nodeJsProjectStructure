4e67fbfae8da2ca21d97858606a38a7b57ea2f1a
otmanedouieb1997 <otmane.douieb.1997@hotmail.com>
2022-01-11 13:51:50 +0100

    add ejs template engine

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express')
-
-
+const ejs = require('ejs')
 
 const app = express()
 
+app.set('view engine', 'ejs')
 
+app.use(express.urlencoded({expanded: true}))
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
 
 app.listen(3000, () => {
     console.log('Server run at port 3000...')
